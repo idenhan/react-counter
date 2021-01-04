6499e8104148c04dfcb1a94b7098eeaba593fc46
Toggle <idenhan129@gmail.com>
2021-01-04 10:47:41 +0900

    Raising and Handling Events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,8 +5,7 @@ class Counter extends Component {
     value: this.props.value,
   };
 
-  handleIncrement = product => {
-    console.log(product);
+  handleIncrement = () => {
     this.setState({ value: this.state.value + 1 });
   };
 
@@ -19,6 +18,12 @@ class Counter extends Component {
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={this.props.onDelete}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
