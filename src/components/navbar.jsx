c20a215a7d586eca5548de80e825227428fed6c9
Toggle <idenhan129@gmail.com>
2021-01-05 16:56:59 +0900

    Destructuring Arguments to simply code

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 // Stateless Functional Component
-const NavBar = props => {
+const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
       <a className="navbar-brand" href="#">
         Navbar{" "}
         <span className="badge badge-pill badge-secondary">
-          {props.totalCounters}
+          {totalCounters}
         </span>
       </a>
     </nav>
