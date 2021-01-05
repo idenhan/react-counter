f8e789784132b2ac8dc738d8ff59ca240169a692
Toggle <idenhan129@gmail.com>
2021-01-05 16:54:30 +0900

    Refact it to Stateless Functional Component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,19 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <a className="navbar-brand" href="#">
-          Navbar{" "}
-          <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+// Stateless Functional Component
+const NavBar = props => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {props.totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default NavBar;
